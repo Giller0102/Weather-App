fbe5a6afd9a933f7b1532bce875591e1fc1eae8a
Gil Lera <hilevskayavaleriya@gmail.com>
2026-09-09 20:36:55 +0300

    feat: create layout of the app and add basic styles

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener('load', (e) => {
+    e.preventDefault();
+
+    const SEARCH_FORM = document.getElementById('search-form');
+    const SEARCH_INPUT = document.getElementById('search-input');
+    const SEARCH_BUTTON = document.getElementById('search-btn');
+
+    
+})
